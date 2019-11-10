e9def29efb94cdd058758c7df4c32301feea73ed
Praditya Nafiis Muhammad <pradityanafiis@gmail.com>
2019-11-10 16:21:48 +0700

    first commit

---
+++ application/controllers/api/Penginapan.php
@@ -0,0 +1,119 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+class Penginapan extends CI_Controller {
+
+    use REST_Controller {
+        REST_Controller::__construct as private __resTraitConstruct;
+    }
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->__resTraitConstruct();
+        $this->load->model('Model_Penginapan', 'penginapan');
+    }
+
+    public function index_get()
+    {
+        $idpenginapan = $this->get('idpenginapan');
+        $iduser = $this->get('iduser');
+
+        if ($idpenginapan != null) {
+            $datapenginapan = $this->penginapan->getPenginapan($idpenginapan, null);
+        } elseif ($iduser != null) {
+            $datapenginapan = $this->penginapan->getPenginapan(null, $iduser);
+        } else {
+            $datapenginapan = $this->penginapan->getPenginapan();
+        }
+
+        if ($datapenginapan) {
+            $this->response([
+                'status' => TRUE,
+                'data' => $datapenginapan
+            ], 200);
+        } else {
+            $this->response([
+                'status' => FALSE,
+                'message' => 'Data penginapan tidak ditemukan.'
+            ], 404);
+        }
+    }
+
+    public function index_post()
+    {
+        $datapenginapan = array(
+            'IDUSER' => $this->post('iduser'),
+            'NAMAPENGINAPAN' => $this->post('namapenginapan'),
+            'ALAMAT' => $this->post('alamat'),
+            'LATITUDE' => $this->post('latitude'),
+            'LONGITUDE' => $this->post('longitude'),
+            'TELEPONPENGINAPAN' => $this->post('teleponpenginapan')
+        );
+
+        if ($this->penginapan->insertPenginapan($datapenginapan)) {
+            $this->response([
+                'status' => TRUE,
+                'message' => 'Berhasil menambahkan penginapan.',
+            ], 201);
+        } else {
+            $this->response([
+                'status' => FALSE,
+                'message' => 'Gagal menambahkan penginapan.',
+            ], 400);
+        }
+    }
+
+    public function index_put()
+    {
+        $idpenginapan = $this->put('idpenginapan');
+
+        $datapenginapan = array(
+            'NAMAPENGINAPAN' => $this->put('namapenginapan'),
+            'ALAMAT' => $this->put('alamat'),
+            'LATITUDE' => $this->put('latitude'),
+            'LONGITUDE' => $this->put('longitude'),
+            'TELEPONPENGINAPAN' => $this->put('teleponpenginapan')
+        );
+
+        if ($this->penginapan->updatePenginapan($datapenginapan, $idpenginapan)) {
+            $this->response([
+                'status' => TRUE,
+                'message' => 'Berhasil ubah penginapan.',
+            ], 200);
+        } else {
+            $this->response([
+                'status' => FALSE,
+                'message' => 'Gagal ubah penginapan.',
+            ], 400);
+        }
+    }
+
+    public function index_delete()
+    {
+        $idpenginapan = $this->delete('idpenginapan');
+
+        if ($idpenginapan == null) {
+            $this->response([
+                'status' => FALSE,
+                'message' => "Gagal, ID penginapan belum dimasukkan."
+            ], 400);
+        } else {
+            if ($this->penginapan->deletePenginapan($idpenginapan)) {
+                $this->response([
+                    'status' => TRUE,
+                    'message' => 'Berhasil hapus penginapan.'
+                ], 200);
+            } else {
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'Gagal, ID penginapan tidak ditemukan.'
+                ]);
+            }
+        }
+    }
+}
---
+++ application/models/Model_Penginapan.php
@@ -0,0 +1,34 @@
+<?php
+
+class Model_Penginapan extends CI_Model {
+    
+    public function getPenginapan($idpenginapan = null, $iduser = null)
+    {
+        if ($idpenginapan != null) {
+            return $this->db->get_where('penginapan', ['IDPENGINAPAN' => $idpenginapan])->result_array();
+        } elseif ($iduser != null) {
+            return $this->db->get_where('penginapan', ['IDUSER' => $iduser])->result_array();
+        } else {
+            return $this->db->get('penginapan')->result_array();
+        }
+    }
+
+    public function insertPenginapan($datapenginapan)
+    {
+        $this->db->insert('penginapan', $datapenginapan);
+        return $this->db->affected_rows();
+    }
+
+    public function updatePenginapan($datapenginapan, $idpenginapan)
+    {
+        $this->db->update('penginapan', $datapenginapan, ['IDPENGINAPAN' => $idpenginapan]);
+        return $this->db->affected_rows();
+    }
+
+    public function deletePenginapan($idpenginapan)
+    {
+        $this->db->delete('penginapan', ['IDPENGINAPAN' => $idpenginapan]);
+        return $this->db->affected_rows();
+    }
+
+}
